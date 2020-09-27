9c4f0088252da6e426fe3d2deee271f5c72f09fd
wushengyu <wushengyu@igengmei.com>
2020-09-27 17:35:01 +0800

    test2 first commit

--- src/main/java/com/lagou/model04/task01/Exec01.java
+++ src/main/java/com/lagou/model04/task01/Exec01.java
@@ -4,5 +4,10 @@ public class Exec01 {
     public static void main(String[] args) {
 //        Thread.sleep(1000);        //编译时异常，Error
 //        System.out.println( 5/0);     //运行是异常：exection
+        jkfdsjka
+                djfska
+                fjdsj
+                        jfjak
+        System.out.println("djkfsaj");
     }
 }
