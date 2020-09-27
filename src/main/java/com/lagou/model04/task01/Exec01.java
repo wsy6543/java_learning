c98faea219db3f75092d4549f25260d89abc15ce
wushengyu <wushengyu@igengmei.com>
2020-09-27 17:32:19 +0800

    fix

---
+++ src/main/java/com/lagou/model04/task01/Exec01.java
@@ -0,0 +1,8 @@
+package com.lagou.model04.task01;
+
+public class Exec01 {
+    public static void main(String[] args) {
+//        Thread.sleep(1000);        //编译时异常，Error
+//        System.out.println( 5/0);     //运行是异常：exection
+    }
+}
